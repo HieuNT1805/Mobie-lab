55ab301307412416b5f04b2eb93bc18658852139
HieuNguyen <91725595+HieuNT1805@users.noreply.github.com>
2023-03-27 22:35:48 +0700

    commit

---
+++ Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    int count=0;
+    Button goToActivityTwo;
+    TextView resultTV, solutionTV;
+    Button buttonC, buttonAC;
+    Button buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEqual;
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        goToActivityTwo=(Button) findViewById(R.id.activity_main);
+        goToActivityTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("KEY_SENDER",Integer.toString(count));
+                startActivity(intent);
+
+            }
+        });
+
+        resultTV=findViewById(R.id.result_tv);
+        solutionTV=findViewById(R.id.solution_tv);
+
+        assignId(buttonC,R.id.button_c);
+        assignId(buttonDivide,R.id.button_divide);
+        assignId(buttonMultiply,R.id.button_multiply);
+        assignId(buttonPlus,R.id.button_plus);
+        assignId(buttonMinus,R.id.button_minus);
+        assignId(buttonEqual,R.id.button_equal);
+        assignId(button0,R.id.button_0);
+        assignId(button1,R.id.button_1);
+        assignId(button2,R.id.button_2);
+        assignId(button3,R.id.button_3);
+        assignId(button4,R.id.button_4);
+        assignId(button5,R.id.button_5);
+        assignId(button6,R.id.button_6);
+        assignId(button7,R.id.button_7);
+        assignId(button8,R.id.button_8);
+        assignId(button9,R.id.button_9);
+        assignId(buttonAC,R.id.button_ac);
+    }
+
+
+
+    void assignId(Button btn,int id){
+        btn=findViewById(id);
+        btn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Button button=(Button) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTV.getText().toString();
+
+        if(buttonText.equals("AC")){
+            solutionTV.setText("");
+            resultTV.setText("0");
+            return;
+        }
+        if(buttonText.equals("=")){
+            solutionTV.setText(resultTV.getText());
+            count++;
+            return;
+        }
+        if(buttonText.equals("C")){
+            dataToCalculate=dataToCalculate.substring(0,dataToCalculate.length()-1);
+        }else {
+            dataToCalculate = dataToCalculate + buttonText;
+        }
+
+        solutionTV.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if(!finalResult.equals("Err")){
+            resultTV.setText(finalResult);
+        }
+    }
+
+    String getResult(String data){
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult = context.evaluateString(scriptable, data, "Javascript",0,null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0","");
+            }
+            return finalResult;
+        }catch (Exception e){
+            return "Err";
+        }
+    }
+}
